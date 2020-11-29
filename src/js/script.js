233b7b24d2ea55643bde22cd39e0c1fd3231a202
EkaterinaSky <esidor27@ya.ru>
2020-11-29 12:38:57 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1100,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/prevArow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/finishArrow.png"></button>'
+    });
+});
