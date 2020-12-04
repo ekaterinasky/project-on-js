44caa99c4b07cf887b9198b7e8697612d23acabc
EkaterinaSky <esidor27@ya.ru>
2020-12-04 13:22:41 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,36 @@ $(document).ready(function () {
         speed: 1100,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/prevArow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/finishArrow.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/finishArrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    autoplay: true
+                }
+            }
+        ]
     });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__contant').removeClass('catalog__contant_active').eq($(this).index()).addClass('catalog__contant_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+
 });
