16b4631083cbac9dee456122368b364dde2d0699
ekaterinasky <74653982+ekaterinasky@users.noreply.github.com>
2020-12-08 20:06:20 +0300

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'ekaterinavitalievnasklyarova@gmail.com';                 // Наш логин
-$mail->Password = 'Nk107m8667L';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
